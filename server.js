9364d7fb531a00d801b8129c1245fbbaeb7f518b
Aditya Tyagi <tyagi.aditya844747@gmail.com>
2017-09-13 23:19:56 +0530

    Version 1.0 Completed

---
+++ server.js
@@ -0,0 +1,35 @@
+var app = require('express')();
+// app becomes the function handler here which we can supply to the http server
+var http = require('http').Server(app);
+
+// initialize a new instance of socket.io by passing the http (the HTTP server) object.
+var io = require('socket.io')(http);
+
+// routes for the home-page
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+//  listen on the connection event for incoming sockets, and I log it to the console.
+io.on('connection', function(socket) {
+
+    console.log('A new user is connected'); // new user is connected
+
+    socket.on('chat message', function(msg) {
+        //console.log('Message: ' + msg);
+
+        //broadcasting the message, including the sender: we use .emit() to broadcast message back to the html->frontend(client)
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('User Disconnected!'); // the user either refreshes the page : disconnect and the re-connect, or user closes the tab
+    });
+});
+
+
+// when the user types in a message, the server gets it as a "chat message" event. 
+
+http.listen(3000, function() {
+    console.log('The application is running on port *:3000');
+});
